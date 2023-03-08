d095e74f150b0d26a862b420491b6670ab107396
Martin Dagarin <martin.dagarin@gmail.com>
2023-03-08 09:05:42 +0100

    Config module

---
+++ backend/src/custom-config/config.schema.ts
@@ -0,0 +1,12 @@
+import * as Joi from 'joi';
+
+export const ConfigSchema: Joi.ObjectSchema = Joi.object().keys({
+  DOCS: Joi.boolean().default(true),
+  DOC_PATH: Joi.string().default('openapi'),
+  NODE_ENV: Joi.string().valid('development', 'test', 'production').default('development'),
+  PORT: Joi.number().default(process.env.PORT || 3002),
+
+  CONFIG: Joi.string(),
+  IGNORE_CONFIG: Joi.boolean(),
+
+});
