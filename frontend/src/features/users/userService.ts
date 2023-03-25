169ccd6eaf9a863997a3e113f6db43e89ba4af72
TineCrnugelj <tine.crnugelj@gmail.com>
2023-03-25 11:46:25 +0100

    ignore this

--- frontend/src/features/users/userService.ts
+++ frontend/src/features/users/userService.ts
@@ -3,6 +3,9 @@ import { LoginData, UserData, UserDataEdit } from "../../classes/userData";
 
 const AUTH_API_URL = 'http://localhost:3000/api/auth';
 const USERS_API_URL = 'http://localhost:3000/api/user';
+const COMMON_PASSWORD_API_URL = 'http://localhost:3000/api/common-password';
+const USER_LOGIN_URL = 'http://localhost:3000/api/user-login';
+
 
 const login = async (userData: LoginData) => {
     const response = await axios.post(`${AUTH_API_URL}/login`, userData);
@@ -64,6 +67,37 @@ const deleteUser = async (userId: string, token: string) => {
     return response.data;
 }
 
+const commonPassword = async (password: {password: string}, token: string) => {
+    const config = {
+        headers: {
+            Authorization: `JWT ${token}`
+        }
+    }
+    const response = await axios.post(COMMON_PASSWORD_API_URL, password, config);
+
+    return response.data;
+}
+
+const setUp2FA = async (userId: string, token: string) => {
+    const config = {
+        headers: {
+            Authorization: `JWT ${token}`
+        }
+    }
+    const response = await axios.post(`${USERS_API_URL}/${userId}/2fa`, {}, config);
+    return response.data;
+}
+
+const getLastLogin = async (userId: string, token: string) => {
+    const config = {
+        headers: {
+            Authorization: `JWT ${token}`
+        }
+    }
+    const response = await axios.get(`${USER_LOGIN_URL}/${userId}/last`, config);
+    return response.data;
+}
+
 const userService = {
     login,
     create,
@@ -71,6 +105,9 @@ const userService = {
     deleteUser,
     logout,
     editUser,
+    commonPassword,
+    setUp2FA,
+    getLastLogin,
 }
 
 export default userService;
