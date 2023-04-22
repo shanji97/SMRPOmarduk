e36a1a86afd42b3becab39f62c47a0b4dc759475
shanji97 <aleksander.kovac97@hotmail.com>
2023-04-22 23:00:46 +0200

    timecomplexity bugfix

--- backend/src/story/dto/update-time-complexity.dto.ts
+++ backend/src/story/dto/update-time-complexity.dto.ts
@@ -2,18 +2,18 @@ import { ApiProperty } from '@nestjs/swagger';
 import * as Joi from 'joi';
 
 export class UpdateStoryTimeComplexityDto {
-    @ApiProperty({
-      description: 'Time complexity.',
-      example: 1,
-      minimum: 0,
-      default: 1,
-      type: Number,
-      required: true
-    })
-    timeComplexity: number;
-  }
-  
-  export const UpdateStoryTimeComplexitySchema = Joi.object().keys({
-    timeComplexity: Joi.number().min(0).required().default(1),
-  });
-  
+  @ApiProperty({
+    description: 'Time complexity.',
+    example: 1,
+    minimum: 0,
+    maximum: 99,
+    default: 1,
+    type: Number,
+    required: true
+  })
+  timeComplexity: number;
+}
+
+export const UpdateStoryTimeComplexitySchema = Joi.object().keys({
+  timeComplexity: Joi.number().min(0).max(99).required().default(1),
+});
