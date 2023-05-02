6e86279cebd93b695fe1c64ad4b6e5cc9d45a000
sivar <sivar.lapajne@gmail.com>
2023-05-02 15:38:33 +0200

    stry modal

--- frontend/src/pages/StoryModal.tsx
+++ frontend/src/pages/StoryModal.tsx
@@ -1,184 +1,144 @@
-import React, { useEffect, useState } from 'react'
-import { Modal, Button, Tab, Card, Nav, CloseButton, FormControl, Table, ListGroup, Row, Form, Col } from "react-bootstrap";
-import { NotificationData, StoryData } from '../classes/storyData';
-
+import React, { useEffect, useState } from "react";
+import {
+  Modal,
+  Button,
+  Tab,
+  Card,
+  Nav,
+  CloseButton,
+  FormControl,
+  Table,
+  ListGroup,
+  Row,
+  Form,
+  Col,
+} from "react-bootstrap";
+import { NotificationData, StoryData } from "../classes/storyData";
 
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 
-import { getAllUsers} from "../features/users/userSlice";
-import { parseJwt } from '../helpers/helpers';
-import { useParams } from 'react-router-dom';
-import { getActiveProject } from '../features/projects/projectSlice';
-import { createNotification, getNotifications, getRejectMessage, reset } from '../features/stories/storyNotificationSlice';
-import { toast } from 'react-toastify';
-import PostNotification from '../components/PostNotification';
-
+import { getAllUsers } from "../features/users/userSlice";
+import { parseJwt } from "../helpers/helpers";
+import { useParams } from "react-router-dom";
+import { getActiveProject } from "../features/projects/projectSlice";
+import {
+  createNotification,
+  getNotifications,
+  getRejectMessage,
+  reset,
+} from "../features/stories/storyNotificationSlice";
+import { toast } from "react-toastify";
+import PostNotification from "../components/PostNotification";
+import { getNotificationReject } from "../features/stories/storySlice";
 
 export interface StoryModalProps {
-    onCancel: VoidFunction
-    show: boolean
-    item: StoryData
+  onCancel: VoidFunction;
+  show: boolean;
+  item: StoryData;
 }
 
-function StoryModal({ 
-    onCancel,
-    show,
-    item,
-}: StoryModalProps) {
-  
-
+function StoryModal({ onCancel, show, item }: StoryModalProps) {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    dispatch(getAllUsers())
-  }, [])
-
-
-
-
-  //demo
-  const initialList = [
-    {
-      id: 1,
-      title: "title1",
-      status: "asigned",
-      user: "matevz",
-      workedTime: 3,
-      remainingTime: 2,
-      estimatedTime: 6
-    },
-    {
-      id: 2,
-      title: "title2",
-      status: "unasigned",
-      user: "janez",
-      workedTime: 4,
-      remainingTime: 1,
-      estimatedTime: 5
-    },
-  ];
-  const [list, setList] = useState(initialList);
-  function handleChange() {
-    // track input field's state
-  }
-
+    dispatch(getAllUsers());
+  }, []);
 
   const handleAdd = (e: any) => {
     e.preventDefault();
-
   };
 
-  let {users, user} = useAppSelector(state => state.users);
+  let { users, user } = useAppSelector((state) => state.users);
 
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [userName, setUserName] = useState("");
 
+  //comments
 
+  const params = useParams();
+  const {
+    message,
+    isError,
+    storiesNotification,
+    rejectMessage,
+    isNotificationError,
+    isNotificationLoading,
+    isNotificationSuccess,
+  } = useAppSelector((state) => state.storyNotifications);
+  const niki = useAppSelector((state) => state.storyNotifications);
+  const { activeProject } = useAppSelector((state) => state.projects);
+  const storyState = useAppSelector((state) => state.stories);
 
+  const [postContent, setPostContent] = useState("");
+  const [currentUser, setUser] = useState({
+    sub: "",
+    sid: "",
+  });
 
+  useEffect(() => {
+    if (user === null) {
+      return;
+    }
+    const token = JSON.parse(localStorage.getItem("user")!).token;
+    const userData = parseJwt(token);
 
+    setUser(userData);
+    setIsAdmin(userData.isAdmin);
+    setUserName(userData.sub);
+    dispatch(getActiveProject());
+  }, []);
 
+  useEffect(() => {
+    dispatch(getNotifications(item.id!));
+    //dispatch(getRejectMessage(item.id!));
+    dispatch(getNotificationReject(item.id!));
+  }, [activeProject]);
 
+  useEffect(() => {
+    if (isError) {
+      toast.error(message);
+    }
 
+    return () => {
+      dispatch(reset());
+    };
+  }, [isError, message]);
 
-const [isAdmin, setIsAdmin]   = useState(false);
-const [userName, setUserName] = useState('');
+  useEffect(() => {
+    if (isNotificationSuccess && !isNotificationLoading) {
+      toast.success(message);
+      dispatch(getNotifications(item.id!));
+    }
+    if (isNotificationError && !isNotificationLoading) {
+      toast.error(message);
+    }
+  }, [isNotificationSuccess, isNotificationError, isNotificationLoading]);
 
-//comments
-
-const params = useParams();
-const {message, isError, storiesNotification, rejectMessage} = useAppSelector(state => state.storyNotifications);
-const {activeProject} = useAppSelector(state => state.projects);
-const [postContent, setPostContent] = useState('');
-const [currentUser, setUser] = useState({
-  sub: '',
-  sid: ''
-});
-
-useEffect(() => {
-  if (user === null) {
-    return;
-  }
-  const token = JSON.parse(localStorage.getItem("user")!).token;
-  const userData = parseJwt(token);
-
-  setUser(userData);
-  setIsAdmin(userData.isAdmin);
-  setUserName(userData.sub);
-  dispatch(getActiveProject());
-}, []);
-
-useEffect(() => {
-  dispatch(getNotifications(item.id!));
-  dispatch(getRejectMessage(item.id!));
-}, [activeProject]);
-
-useEffect(() => {
-  if (isError) {
-    toast.error(message);
-  }
-
-  return () => {
-    dispatch(reset());
-  }
-}, [isError, message]);
-
-const postContentChanged = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-  setPostContent(e.currentTarget.value);
-}
-
-
-const submitNewPost = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-  const newPost: NotificationData = {
-    description: postContent,
-    storyId: item.id!
+  const postContentChanged = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setPostContent(e.currentTarget.value);
   };
-  dispatch(createNotification(newPost));
-  toast.info('Post created!');
-  setPostContent('');
-}
 
+  const submitNewPost = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newPost: NotificationData = {
+      description: postContent,
+      storyId: item.id!,
+    };
+    dispatch(createNotification(newPost));
+    toast.info("Post created!");
+    setPostContent("");
+    //dispatch(getNotifications(item.id!));
+  };
 
-
-/*
-
- <Card border={"info"}>
-                    <Card.Body>
-                      <Row>
-                        <Col>
-                          <Card.Text>{content}</Card.Text>
-                        </Col>
-                        
-                        
-                      </Row>
-                      </Card.Body>
-                    </Card>
-
-
-                {rejectMessage.map((post, i) => {
-                    return <PostNotification
-                  key={i}
-                  id={post.id!}
-                  content={post.notificationText}
-                  author={post.authorName}
-                  created={post.created!}
-                  user={currentUser}
-                  approved={post.approved}
-  
-              />
-      })} 
-
-      */
-    return (
-    
-        <Modal
-        
-        show={show} 
-        
-        onHide={onCancel}
-        backdrop="static"
-        keyboard={false}
-        size="xl"
-        centered>
+  return (
+    <Modal
+      show={show}
+      onHide={onCancel}
+      backdrop="static"
+      keyboard={false}
+      size="xl"
+      centered
+    >
       <Tab.Container id="left-tabs-example" defaultActiveKey="first">
         <Card>
           <Card.Header className="d-flex align-items-center">
@@ -190,64 +150,69 @@ const submitNewPost = (e: React.FormEvent<HTMLFormElement>) => {
                 <Nav.Link eventKey="second">Comments</Nav.Link>
               </Nav.Item>
             </Nav>
-            <CloseButton className="ms-auto" onClick={onCancel}/>
+            <CloseButton className="ms-auto" onClick={onCancel} />
           </Card.Header>
           <Card.Body>
             <Tab.Content>
               <Tab.Pane eventKey="first">
                 <Card.Title>{item.title}</Card.Title>
                 <Card.Text>
-
-
-               
                   {item.description}
+
+                  {storyState.notificationReject.map((post, i) => {
+                    return (
+                      <PostNotification
+                        key={i}
+                        id={post.id!}
+                        content={post.notificationText}
+                        author={post.authorName}
+                        created={post.created!}
+                        user={currentUser}
+                        approved={post.approved}
+                      />
+                    );
+                  })}
                 </Card.Text>
               </Tab.Pane>
               <Tab.Pane eventKey="second">
+                {storiesNotification.map((post, i) => {
+                  return (
+                    <PostNotification
+                      key={i}
+                      id={post.id!}
+                      content={post.notificationText}
+                      author={post.authorName}
+                      created={post.created!}
+                      user={currentUser}
+                      approved={post.approved}
+                    />
+                  );
+                })}
 
-              
-     
-
-       {storiesNotification.map((post, i) => {
-        return <PostNotification
-                  key={i}
-                  id={post.id!}
-                  content={post.notificationText}
-                  author={post.authorName}
-                  created={post.created!}
-                  user={currentUser}
-                  approved={post.approved}
-  
-              />
-      })} 
-
-      <Form onSubmit={submitNewPost}>
-        <Form.Group className="mb-3" controlId="postContent" >
-          <Form.Label>Write a new post</Form.Label>
-          <Form.Control
-            as='textarea'
-            
-            value={postContent}
-            onChange={postContentChanged}
-          />
-          <Button type='submit' disabled={postContent === ''} style={{ marginTop: '.5rem' }}>Post</Button>
-        </Form.Group>
-      </Form>
- 
-                
-
-                
+                <Form onSubmit={submitNewPost}>
+                  <Form.Group className="mb-3" controlId="postContent">
+                    <Form.Label>Write a new post</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      value={postContent}
+                      onChange={postContentChanged}
+                    />
+                    <Button
+                      type="submit"
+                      disabled={postContent === ""}
+                      style={{ marginTop: ".5rem" }}
+                    >
+                      Post
+                    </Button>
+                  </Form.Group>
+                </Form>
               </Tab.Pane>
             </Tab.Content>
-            </Card.Body>
+          </Card.Body>
         </Card>
       </Tab.Container>
     </Modal>
-    
-    )
+  );
 }
-
-
-
 
 export default StoryModal;
